5518e59b4d948b5270f6e4ee64297bdbcabc61b6
najraham <anishamaharjan4@gmail.com>
2020-12-24 19:06:07 +0545

    CORS headers added

--- index.js
+++ index.js
@@ -18,6 +18,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+//CORS
+app.all('/*', function(req, res, next) {
+    // CORS headers
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    // Set custom headers for CORS
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if (req.method == 'OPTIONS') {
+      res.status(200).end();
+    } else {
+      next();
+    }
+  }); 
+
 // routes
 app.use ('/api/todos', require('./route/api/todos'));
 
